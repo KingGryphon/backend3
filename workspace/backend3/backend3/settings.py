169a2c593b77a75acd7d56c678c9c76c91e490db
Archer Bergmann <archerbergmann@The-Kraken.ht.home>
2024-08-27 17:13:46 -0400

    grrr

--- workspace/backend3/backend3/settings.py
+++ workspace/backend3/backend3/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +79,15 @@ WSGI_APPLICATION = "backend3.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "backend3",
+        'USER': 'root',
+        'PASSWORD': 'password123!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
